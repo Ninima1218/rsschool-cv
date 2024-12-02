bef726d6118dc387c0ed57662f96ebcbaa7faa43
Ninima1218 <ninima1218@gmail.com>
2024-12-03 01:51:23 +0400

    add json file

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -1,18 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.querySelector('.slider');
-  const prevButton = document.querySelector('.prev');
-  const nextButton = document.querySelector('.next');
+    const slider = document.querySelector('.slider');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const burgerMenuContent = document.querySelector('.burger-menu-content');
 
-  let slideIndex = 0;
-  const slideWidth = document.querySelector('.slide').offsetWidth; // Ширина одного слайда
+    const toggleBurgerMenu = () => {
+        burgerMenu.classList.toggle('active');
+    };
 
-  prevButton.addEventListener('click', () => {
-    slideIndex = (slideIndex - 1 + 4) % 4; // 4 - количество слайдов
-    slider.style.transform = `translateX(-${slideIndex * slideWidth}px)`; // Прокручиваем слайдер влево
-  });
+    burgerMenu.addEventListener('click', toggleBurgerMenu);
 
-  nextButton.addEventListener('click', () => {
-    slideIndex = (slideIndex + 1) % 4; // 4 - количество слайдов
-    slider.style.transform = `translateX(-${slideIndex * slideWidth}px)`; // Прокручиваем слайдер вправо
-  });
+    let slideIndex = 0;
+    const slideWidth = document.querySelector('.slide').offsetWidth;
+
+    const updateSlide = (index) => {
+        slider.style.transform = `translateX(-${index * slideWidth}px)`;
+    };
+
+    const showPrevSlide = () => {
+        slideIndex = (slideIndex - 1 + 4) % 4;
+        updateSlide(slideIndex);
+    };
+
+    const showNextSlide = () => {
+        slideIndex = (slideIndex + 1) % 4; 
+        updateSlide(slideIndex);
+    };
+
+    prevButton.addEventListener('click', showPrevSlide);
+    nextButton.addEventListener('click', showNextSlide);
 });
+
