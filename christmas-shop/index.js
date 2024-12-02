3eb834e501c22fe20b47b861e7d66cbe73f7651c
Ninima1218 <ninima1218@gmail.com>
2024-12-02 21:53:20 +0400

    add index.js and gifts.js files

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -1,21 +1,23 @@
-const slides = document.querySelector('.slides');
-const slide = document.querySelectorAll('.slide');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-let index = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navUl = document.querySelector('.nav-list');
+    
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        navUl.classList.toggle('active');
+    });
 
-function showSlide(n) {
-    index = (n + slide.length) % slide.length; 
-    slides.style.transform = `translateX(${-index * 100}%)`;
-}
+    const slides = document.querySelector('.slides');
+    const slide = document.querySelectorAll('.slide');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    let index = 0;
 
-prevBtn.addEventListener('click', () => showSlide(index - 1));
-nextBtn.addEventListener('click', () => showSlide(index + 1));
+    function showSlide(n) {
+        index = (n + slide.length) % slide.length; 
+        slides.style.transform = `translateX(${-index * 100}%)`;
+    }
 
-const burgerMenu = document.querySelector('.burger-menu');
-const navUl = document.querySelector('nav ul');
-
-burgerMenu.addEventListener('click', () => {
-    burgerMenu.classList.toggle('active');
-    navUl.classList.toggle('active');
+    prevBtn.addEventListener('click', () => showSlide(index - 1));
+    nextBtn.addEventListener('click', () => showSlide(index + 1));
 });
